3e7c87666a387eabbafa9ad368c03d9eba5c307e
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-01-24 19:14:37 -0500

    Problem solved

--- src/index.js
+++ src/index.js
@@ -38,9 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        let subExpr = expr.substring(i, i + 10);
+        let signs = "";
+        if (subExpr === "**********") {
+            result += " ";
+        } else {
+            for (let j = 0; j < subExpr.length; j += 2) {
+                if (subExpr.substring(j, j + 2) == "10") {
+                    signs += ".";
+                } else if (subExpr.substring(j, j + 2) == "11") {
+                    signs += "-";
+                }
+                console.log(signs);
+            }
+            result += MORSE_TABLE[signs];
+        }
+    }
+    return result;
 }
+
+
+
 
 module.exports = {
     decode
 }
+
